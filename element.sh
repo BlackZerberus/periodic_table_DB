4fb60fceef147999b9ce7029c034c21f0ce29231
BlackZerberus <c.e.chappa@gmail.com>
2022-06-02 21:15:15 +0000

    feat: add element searching

--- element.sh
+++ element.sh
@@ -4,4 +4,23 @@
 if [[ -z $1 ]]
 then
   echo "Please provide an element as an argument."
+else
+  case $1 in
+    # search by Symbol
+    [A-Z] | [A-Z][a-z])
+      echo "buscar por simbolo"
+    ;;
+    # search by atomic number
+    [0-9]*)
+      echo "buscar por numero atomico"
+    ;;
+    # search by name
+    [A-Z][a-z]*)
+      echo "buscar por nombre"
+    ;;
+    # if no matches exist:
+    *)
+      echo "Please provide an element as an argument."
+    ;;
+  esac
 fi
